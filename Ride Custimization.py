2382161110ff05872f6f2d1f01772d48d569eaa3
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-04-19 18:40:53 -0700

    Ride_Custimization

---
+++ Ride Custimization.py
@@ -0,0 +1,31 @@
+print("1.Bike")
+print("2.Car")
+vehicle = int(input("Choose which vehicle you want by typing in the number:"))
+
+if vehicle == 1:
+    print("You have selected bike")
+    print("Which type of bike you want")
+    print("1.Scooty")
+    print("2.Scooter")
+    which_bike = int(input("Please enter the number of the bike you want:"))
+    if which_bike == 1:
+        print("You have selected Scooty")
+    elif which_bike == 2:
+        print("You have selected Scooter")
+    else:
+        print("Invalid Input")
+elif vehicle == 2:
+    print("You have selected car")
+    print("Which type of car you want")
+    print("1.Sedan")
+    print("2.XUV")
+    which_car = int(input("Please enter the number of the car you want:"))
+    if which_car == 1:
+        print("You have chosen Sedan")
+    elif which_car == 2:
+        print("You have chosen XUV")
+    else:
+        print("Invalid Input")
+else:
+    print("Invalid Input")
+  
